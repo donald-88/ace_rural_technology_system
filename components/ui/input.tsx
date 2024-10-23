de2dec779632e479b1290600f653fff761490050
McDonald Namba <nambamcdonald@gmail.com>
2024-10-23 16:44:59 +0200

    changed input component

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -2,9 +2,9 @@ import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
-export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+export type InputProps = React.InputHTMLAttributes<HTMLInputElement>
 
+// The rest of your code remains the same
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
     return (
@@ -20,6 +20,6 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
     )
   }
 )
-Input.displayName = "Input"
 
+Input.displayName = "Input"
 export { Input }
