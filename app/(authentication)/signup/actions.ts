975253f254556c97d2800544d99f5db191e91a29
McDonald Namba <nambamcdonald@gmail.com>
2024-10-23 16:12:06 +0200

    removed mongodb dependency

--- app/(authentication)/signup/actions.ts
+++ app/(authentication)/signup/actions.ts
@@ -1,19 +1,10 @@
 "use server"
 
-import { connectMongoDB } from "@/lib/mongodb"
-import { Users } from "@/models/teamMembers"
 
 export const signUpFormAction = async (prevState: any, formData: FormData) => {
     const email = formData.get("email") as string
     const password = formData.get("password") as string
     const confirmPassword = formData.get("confirmPassword") as string
-
-    await connectMongoDB()
-
-    await Users.create({
-        email: email,
-        password: password,
-    })
 
     console.log(email, password, confirmPassword)
 
