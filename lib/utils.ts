bdfd3446f6337e119af7dc81eecf0b7d518485b4
McDonald Namba <nambamcdonald@gmail.com>
2024-10-24 02:14:12 +0200

    fixed parse type error

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,8 +1,9 @@
 import { clsx, type ClassValue } from "clsx"
+import { Models } from "node-appwrite";
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export const parseStringify = (value: any) => JSON.parse(JSON.stringify(value));
+export const parseStringify = (value: Models.Document[]) => JSON.parse(JSON.stringify(value));
