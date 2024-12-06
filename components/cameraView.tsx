f03d6e899ac1ca1d7fc369d9999ba7292d1b7def
McDonald Namba <nambamcdonald@gmail.com>
2024-12-06 15:21:20 +0200

    testing video surveillance

--- components/cameraView.tsx
+++ components/cameraView.tsx
@@ -1,30 +1,14 @@
 "use client"
-import { useState, useEffect } from 'react'
-import dynamic from 'next/dynamic'
-
-// Dynamically import ReactPlayer with SSR disabled
-const ReactPlayer = dynamic(() => import('react-player'), {
-    ssr: false
-})
 
 const CameraView = () => {
-    // Use state to ensure client-side rendering
-    const [isClient, setIsClient] = useState(false)
-
-    useEffect(() => {
-        setIsClient(true)
-    }, [])
-
-    if (!isClient) return null
 
     return (
-        <div className="w-full h-full">
-            <ReactPlayer
-                width="100%"
-                height="100%"
-                url={"http://192.168.1.119:4000"}
-                playing={true}
+        <div className="w-full h-full rounded-lg overflow-hidden">
+            <video
+                src="http://168.253.229.53:80/video1.mjpg" // Replace with your camera's HTTP stream URL
                 controls
+                autoPlay
+                style={{ width: '100%', height: '100%' }}
             />
         </div>
     )
@@ -33,22 +17,22 @@ const CameraView = () => {
 export default CameraView
 
 
-    // < div className = "bg-gray-300 rounded-xl" >
-    //     <div className="flex flex-col place-content-between h-full">
-    //         <div className="flex justify-between items-center p-3">
-    //             <div className="flex gap-2 text-white">
-    //                 <WifiHigh size="20" />
-    //                 <BatteryFullIcon size="20" />
-    //             </div>
-    //             <div className="flex gap-2">
-    //                 <p className="text-white">86</p>
-    //                 <MoreHorizontal size="20" className="text-white" />
-    //             </div>
-    //         </div>
+// < div className = "bg-gray-300 rounded-xl" >
+//     <div className="flex flex-col place-content-between h-full">
+//         <div className="flex justify-between items-center p-3">
+//             <div className="flex gap-2 text-white">
+//                 <WifiHigh size="20" />
+//                 <BatteryFullIcon size="20" />
+//             </div>
+//             <div className="flex gap-2">
+//                 <p className="text-white">86</p>
+//                 <MoreHorizontal size="20" className="text-white" />
+//             </div>
+//         </div>
 
-    //         <div className="flex justify-between p-3 text-white">
-    //             <p>Camera {cameraIndex}</p>
-    //             <p>15-05-2024  12:18 PM</p>
-    //         </div>
-    //     </div>
-    //     </ >
+//         <div className="flex justify-between p-3 text-white">
+//             <p>Camera {cameraIndex}</p>
+//             <p>15-05-2024  12:18 PM</p>
+//         </div>
+//     </div>
+//     </ >
