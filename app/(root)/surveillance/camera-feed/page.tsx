c9d84134ab244dd77bbec3664e528d348ed58d70
McDonald Namba <nambamcdonald@gmail.com>
2024-12-11 16:44:28 +0200

    video surveillance

--- app/(root)/surveillance/camera-feed/page.tsx
+++ app/(root)/surveillance/camera-feed/page.tsx
@@ -6,10 +6,10 @@ const CameraFeed = () => {
     return (
         <section className="p-4 h-full">
             <div className="grid grid-cols-2 gap-4 w-full h-full">
-                <CameraView  />
-                <CameraView  />
-                <CameraView  />
-                <CameraView />
+                <CameraView url="https://g2.ipcamlive.com/player/player.php?alias=6759a0ec585ac" />
+                <CameraView url="https://g2.ipcamlive.com/player/player.php?alias=6759a28163422" />
+                <CameraView url="https://g2.ipcamlive.com/player/player.php?alias=6759a335f2915" />
+                <CameraView url="https://g2.ipcamlive.com/player/player.php?alias=6759a3f2016ab" />
             </div>
         </section>
     )
--- components/cameraView.tsx
+++ components/cameraView.tsx
@@ -1,38 +1,17 @@
 "use client"
 
-const CameraView = () => {
+import ReactPlayer from 'react-player'
 
+interface CameraViewProps {
+    url: string
+}
+
+const CameraView = ({ url }: CameraViewProps) => {
     return (
         <div className="w-full h-full rounded-lg overflow-hidden">
-            <video
-                src="http://168.253.229.53:80/video1.mjpg" // Replace with your camera's HTTP stream URL
-                controls
-                autoPlay
-                style={{ width: '100%', height: '100%' }}
-            />
+            <iframe src={url} width="100%" height="100%" allowFullScreen> </iframe>
         </div>
     )
 }
 
 export default CameraView
-
-
-// < div className = "bg-gray-300 rounded-xl" >
-//     <div className="flex flex-col place-content-between h-full">
-//         <div className="flex justify-between items-center p-3">
-//             <div className="flex gap-2 text-white">
-//                 <WifiHigh size="20" />
-//                 <BatteryFullIcon size="20" />
-//             </div>
-//             <div className="flex gap-2">
-//                 <p className="text-white">86</p>
-//                 <MoreHorizontal size="20" className="text-white" />
-//             </div>
-//         </div>
-
-//         <div className="flex justify-between p-3 text-white">
-//             <p>Camera {cameraIndex}</p>
-//             <p>15-05-2024  12:18 PM</p>
-//         </div>
-//     </div>
-//     </ >
