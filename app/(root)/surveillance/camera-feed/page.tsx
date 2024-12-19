47edd527478490e2489555dbb7fabba92d987a07
uchindami <guchindami@gmail.com>
2024-12-19 16:15:19 +0200

    camera feed design

--- app/(root)/surveillance/camera-feed/page.tsx
+++ app/(root)/surveillance/camera-feed/page.tsx
@@ -15,8 +15,8 @@ const CameraFeed = () => {
         <section className="p-4 h-full">
             <div className="grid grid-cols-2 gap-4 w-full h-full">
                 {cameraIds.map((cameraId) => (
-                    <div key={cameraId} className="w-full h-full">
-                        <video controls autoPlay muted className="w-full h-full">
+                    <div key={cameraId} className="w-full h-full ">
+                        <video controls autoPlay muted className="w-full h-full rounded-lg">
                             <source src={`/api/stream?camera=${cameraId}`} type="video/mp4" />
                             Your browser does not support the video tag.
                         </video>
