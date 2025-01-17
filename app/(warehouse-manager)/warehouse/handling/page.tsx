e9731402e6e355b91cbbb2e464da78308db25c8e
Mcdonald Namba <nambamcdonald@gmail.com>
2025-01-17 12:08:26 +0200

    restructured handling and dispatch pages

--- app/(warehouse-manager)/warehouse/handling/page.tsx
+++ app/(warehouse-manager)/warehouse/handling/page.tsx
@@ -5,127 +5,16 @@ import React from 'react'
 
 const WarehouseHandling = () => {
     return (
-        <form className="grid gap-2 p-4">
-
-            <p className="font-bold">Depositors Details</p>
-
-            <div className="grid grid-cols-2 gap-2">
-                <CustomFormField
-                    placeholder=""
-                    name="name"
-                    label="Customer Name"
-                    id="name"
-                    fieldtype={FormFieldType.INPUT} />
-                <CustomFormField
-                    placeholder=""
-                    name="bank"
-                    label="Bank Name"
-                    id="bank"
-                    fieldtype={FormFieldType.SELECT}
-                    options={['Standard Bank', 'National Bank', 'FDH Bank', 'First Capital Bank', 'NBS Bank', 'Centenary Bank', 'Ecobank']}
-                />
-                <CustomFormField
-                    placeholder=""
-                    name="accountName"
-                    label="Account Name"
-                    id="accountName"
-                    fieldtype={FormFieldType.INPUT}
-                />
-                <CustomFormField
-                    placeholder=""
-                    name="accountNumber"
-                    label="Account Number"
-                    id="accountNumber"
-                    fieldtype={FormFieldType.NUMBER}
-                />
-                <CustomFormField
-                    placeholder=""
-                    name="amount"
-                    label="Amount"
-                    id="amount"
-                    fieldtype={FormFieldType.NUMBER}
-                />
+        <section className='px-4 pt-10 h-full w-full flex flex-col items-center'>
+            <div className='flex gap-2 lg:w-[720px] h-min mb-2'>
+                <CustomFormField fieldtype={FormFieldType.SEARCH} placeholder='Search intake id' name={'search'} id={'search'} />
+                <Button>Search</Button>
             </div>
-
-            <div className="h-2" />
-
-            <p className="font-bold">Commodity Details</p>
-
-            <div className="grid grid-cols-2 gap-2">
-                <CustomFormField
-                    placeholder=""
-                    name="commodity"
-                    label="Commodity"
-                    id="commodity"
-                    fieldtype={FormFieldType.INPUT}
-                />
-                <CustomFormField
-                    placeholder=""
-                    name="variety"
-                    label="Variety"
-                    id="variety"
-                    fieldtype={FormFieldType.INPUT}
-                />
-                <CustomFormField
-                    placeholder=""
-                    name="grade"
-                    label="Grade"
-                    id="grade"
-                    fieldtype={FormFieldType.NUMBER}
-                />
-                <CustomFormField
-                    placeholder=""
-                    name="price"
-                    label="Price/Kg"
-                    id="price"
-                    fieldtype={FormFieldType.NUMBER}
-                />
-                <CustomFormField
-                    placeholder=""
-                    name="grossWeight"
-                    label="Gross Weight"
-                    id="grossWeight"
-                    fieldtype={FormFieldType.NUMBER}
-                />
-                <CustomFormField
-                    placeholder=""
-                    name="deductions"
-                    label="Deductions"
-                    id="deductions"
-                    fieldtype={FormFieldType.NUMBER}
-                />
-                <CustomFormField
-                    placeholder=""
-                    name="netWeight"
-                    label="Net Weight"
-                    id="netWeight"
-                    fieldtype={FormFieldType.NUMBER}
-                />
-                <CustomFormField
-                    placeholder=""
-                    name="moistureIn"
-                    label="Moisture In"
-                    id="moistureIn"
-                    fieldtype={FormFieldType.NUMBER}
-                />
-                <CustomFormField
-                    placeholder=""
-                    name="noOfBags"
-                    label="No of Bags"
-                    id="noOfBags"
-                    fieldtype={FormFieldType.NUMBER}
-                />
-                <CustomFormField
-                    placeholder=""
-                    name="bagsReturned"
-                    label="Bags Returned"
-                    id="bagsReturned"
-                    fieldtype={FormFieldType.NUMBER}
-                />
+            <p className='text-muted-foreground'>Scan the barcode on the bag to get the reference id</p>
+            <div className='w-full h-full flex justify-center items-center'>
+                <p className='text-muted-foreground'>No handling results were found</p>
             </div>
-            <div className="h-2" />
-            <Button type="submit">Submit</Button>
-        </form>
+        </section>
     )
 }
 
