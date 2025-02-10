28b251c7eedbb74835adfe21512412991946b106
McDonald Namba <nambamcdonald@gmail.com>
2025-02-10 11:23:15 +0200

    using http instead of async

--- db/index.ts
+++ db/index.ts
@@ -1,5 +1,3 @@
-import { neon } from '@neondatabase/serverless';
-import { drizzle } from 'drizzle-orm/neon-serverless';
+import { drizzle } from 'drizzle-orm/neon-http';
 
-const sql = neon<boolean, boolean>(process.env.DATABASE_URL!)
-export const db = drizzle({ client: sql });
+export const db = drizzle(process.env.DATABASE_URL!);
